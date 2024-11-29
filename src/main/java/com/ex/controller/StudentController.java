8a9ed1bf208823406986e62c6aa98aebd8fe9d0a
yuequanhuabai <yuequanhuabai@qq.com>
2024-11-29 23:07:10 +0800

    delete redundant

--- src/main/java/com/ex/controller/StudentController.java
+++ src/main/java/com/ex/controller/StudentController.java
@@ -16,34 +16,17 @@ import java.util.List;
 @Controller
 public class StudentController {
 
-    Logger logger = LoggerFactory.getLogger(StudentController.class);
+    public static final Logger logger = LoggerFactory.getLogger(StudentController.class);
 
     @Resource
     private StudentService studentService;
 
 
-
-
     @PostMapping("/queryStus")
     public List<Student> queryStudents() {
-
-//        DynamicDataSourceContextHolder.clearDataSource();
-//        DynamicDataSourceContextHolder.setDataSource("test1");
         List<Student> students = studentService.queryStus("zhangsan", 10, 1);
         logger.info("students: " + students);
-//        DataSourceModel model = new DataSourceModel();
-//        model.setCreateTime(new Date());
-//        model.setCreator("zhangsan");
-//        model.setDsid(UUID.randomUUID().toString().replaceAll("-",""));
-//        model.setDsName("test2");
-//        model.setDsType("mysql");
-//        model.setUsername("root");
-//        model.setPassword("root");
-//        model.setDriverClassName("com.mysql.cj.jdbc.Driver");
-//        model.setUrl("jdbc:mysql://localhost:3306/test2?allowPublicKeyRetrieval=true&useUnicode=true&characterEncoding=utf8&characterSetResults=utf8&useSSL=false");
-//
-//        int i = dataSourceModelDao.insertDataSourceModel(model);
-//        System.out.printf("i:"+i);
+
         return students;
     }
 
@@ -60,12 +43,9 @@ public class StudentController {
     }
 
 
-
-
     // 官网:
     // mime 类型
     // contextType: https://www.iana.org/assignments/media-types/media-types.xhtml
-
 
 
 }
--- src/main/java/com/ex/dao/StudentDao.java
+++ src/main/java/com/ex/dao/StudentDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 
 public interface StudentDao {
-    //    com.ex.dao.StudentDao.queryStus
+
     List<Student> queryStus();
 
     Integer insertStu(Student student);
