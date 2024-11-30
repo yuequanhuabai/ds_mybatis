4b36a91d5f7320052fc16c771da438c4eb680171
baiyingwaming <baiyingwaming@foxmail.com>
2024-11-30 17:55:43 +0800

    11

--- src/main/java/com/ex/dao/mysql/StudentDaoMYSQL.java
+++ src/main/java/com/ex/dao/mysql/StudentDaoMYSQL.java
@@ -1,14 +1,23 @@
 package com.ex.dao.mysql;
 
-//@Repository
-//public class StudentDaoMYSQL implements StudentDao {
-//
-//    //    @Resource(name = "sqlSessionTemplateMysql")
-//    private SqlSessionTemplate sqlSessionTemplate;
-//
-//    @Override
-//    public List<Student> queryStus(Map<String, Object> map) {
-//        return sqlSessionTemplate.selectList("StudentMapperMysql.queryStus", map);
-//    }
-//
-//}
+import com.ex.dao.StudentDao;
+import com.ex.entity.Student;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+
+@Repository
+public class StudentDaoMYSQL implements StudentDao {
+
+    //    @Resource(name = "sqlSessionTemplateMysql")
+    private SqlSessionTemplate sqlSessionTemplate;
+
+    @Override
+    public List<Student> queryStus(Map<String, Object> map) {
+        return sqlSessionTemplate.selectList("StudentMapperMysql.queryStus", map);
+    }
+
+}
